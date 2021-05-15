b1fcf57ecc861909542a9176b4a3b94cbf79ee33
maruf786 <maruf@modello.my>
2021-05-15 14:37:36 +0600

    component test

--- resources/js/vue/app.vue
+++ resources/js/vue/app.vue
@@ -1,11 +1,30 @@
 <template>
     <div>
         <p>Hello</p>
+        <div v-if="data.length >0">
+            <div v-for="d in data" :key="d.id">
+                <h3>{{d.name}}</h3>
+                <p>Comleted: <input type="checkbox"  :checked="!d.completed"/></p>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
+data(){
+    return{
+        data:null
+    }
+},
+created(){
+    axios.get('/api/items')
+    .then(res=>{
+        this.data = res.data
+    })
 
+    console.log(this.data)
+}
 }
 </script>
