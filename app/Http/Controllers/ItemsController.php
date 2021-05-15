bb40a30c0a75375799925dd1fc4af24c8ad6d2d7
maruf786 <maruf@modello.my>
2021-05-15 13:36:39 +0600

    Item update

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Item;
-
+use Illuminate\Support\Carbon;
 
 class ItemsController extends Controller
 {
@@ -26,35 +26,13 @@ class ItemsController extends Controller
      */
     public function store(Request $request)
     {
-       
+
         $item = new Item;
 
         $item->name = $request->item["name"];
         $item->save();
 
         return $item;
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
     }
 
     /**
@@ -66,7 +44,17 @@ class ItemsController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $item = Item::find($id);
+
+        if($item){
+            $item->completed = $request->item['completed'] ? true : false;
+            $item->completed_at = $request->item['completed'] ? Carbon::now() : null;
+            $item->save();
+
+            return $item;
+        }
+
+        return "Item not found";
     }
 
     /**
